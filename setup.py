1bddae8e18a4925c253f3371cd262b61e1f4112e
riosb <riosbap@gmail.com>
2015-06-22 17:19:22 +0200

    moving files to another dir and changing some setup features

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='holidays',
+setup(name='usefulholidays',
       version='0.1',
       description='The disrupting package to check the school calendar :) ',
-      url='https://github.com/WRPLA/holidays.git',
+      url='https://github.com/WRPLA/usefulholidays',
       author='brios',
       license='T4T-Rios',
-      packages=['holidays'],
+      packages=['usefulholidays'],
       zip_safe=False)
