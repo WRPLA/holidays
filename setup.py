7d16d4c70f73dd0163eae2e1251b21c0235eaf8b
riosb <riosbap@gmail.com>
2015-06-22 17:46:27 +0200

    package now recognized in site-packages, moving a dir was needed

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-
+import shutil
 setup(name='usefulholidays',
       version='0.1',
       description='The disrupting package to check the school calendar :) ',
@@ -8,3 +8,6 @@ setup(name='usefulholidays',
       license='T4T-Rios',
       packages=['usefulholidays'],
       zip_safe=False)
+print('lol')
+shutil.move('/usr/local/lib/python3.4/site-packages/usefulholidays-0.1-py3.4.egg/usefulholidays',
+          '/usr/local/Cellar/python3/3.4.3/Frameworks/Python.framework/Versions/3.4/lib/python3.4/site-packages')
